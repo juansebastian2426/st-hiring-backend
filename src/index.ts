4f6b6d10179c0adfe011a097ce12b17082b06fd1
tuttodev <jsebas2426@gmail.com>
2024-07-26 05:11:11 -0500

    feat: add cors into server

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,22 @@ const TicketDAL = createTicketDAL(Knex);
 
 const app = express();
 
+// way without cors library
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:5173', 'https://midominio.com'];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  } else {
+    res.header('Access-Control-Allow-Origin', 'null');
+  }
+
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
